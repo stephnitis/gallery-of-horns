6837f0c175d5a2f10685b3c5b2af4ea7ed2a2b77
Stephanie Hill <stephni.hill@gmail.com>
2022-06-14 15:40:00 -0700

    data display from the array a short haiku by Stephanie Hill

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -4,12 +4,17 @@ class Hornedbeast extends React.Component {
 
   render() {
     return (
-      <>    
-         <article>
-          <h2>{this.props.name}</h2>
+      <>
+        <article>
+          <h2>{this.props.title}</h2>
+          <img
+            src={this.props.image_url}
+            alt={this.props.keyword}
+            title={this.props.title}
+          />
           <p>{this.props.description}</p>
-          </article>
-          
+        </article>
+
       </>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import Hornedbeast from './Hornedbeast';
+import data from './data.json';
 
 class Main extends React.Component {
 
   render() {
+    let hornBeast = data.map(beast => {
+      return (
+        <Hornedbeast
+          title={beast.title}
+          image_url={beast.image_url}
+          description={beast.description}
+        />
+      )
+    })
+
     return (
-      <>
-        <main>
-          <Hornedbeast 
-            name="Narwhal"
-            description="A unicorn and narwhal nuzzle their horns in an expression of forbidden love." />
-          <Hornedbeast 
-            name="Rhinocerosaurus and Young"
-            description="A single mother rummages the Savannah after months without child support."
-          />
-          <Hornedbeast
-            name="Psychedelic Unicorn"
-            description="A unicorn colt who has stumbled into a patch of hongos alucinógenos."
-          />
-        </main>
-      </>
-    );
+      <main>
+        {hornBeast}
+      </main >
+    )
   }
 }
 
