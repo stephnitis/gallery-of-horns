4d02ded15b585dce27aa487cfd4cb12708477224
Stephanie Hill <stephni.hill@gmail.com>
2022-06-14 18:29:05 -0700

    just some final touches for the day

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Hornedbeast.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
 
@@ -22,15 +24,21 @@ class Hornedbeast extends React.Component {
     return (
       <>
         <article>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={this.props.image_url} />
-            <Card.Body>
-              <Card.Title>{this.props.title}</Card.Title>
-              <Card.Text>{this.state.likes} 💙</Card.Text>
-              <Card.Text>{this.props.description}</Card.Text>
-              <Button variant="primary" onClick={this.handleLikes}>Like</Button>
-            </Card.Body>
-          </Card>
+          <Row xs={1} md={3} className="g-4">
+            {Array.from({ length: 1 }).map((_, idx) => (
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={this.props.image_url} />
+                  <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Card.Text>{this.state.likes} 💙</Card.Text>
+                    <Button variant="primary" onClick={this.handleLikes}>Like</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </article>
 
 
